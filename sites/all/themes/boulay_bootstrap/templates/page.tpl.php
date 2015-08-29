e3b45a198e71f102750ee1ad9ce19c6c15c0be74
Kyle Bulloch <kylekbulloch@gmail.com>
2015-08-28 17:43:02 -0700

    Switch to blue texture bg, add banner region/view/block

--- sites/all/themes/boulay_bootstrap/templates/page.tpl.php
+++ sites/all/themes/boulay_bootstrap/templates/page.tpl.php
@@ -118,6 +118,7 @@
       <?php if (!empty($site_slogan)): ?>
         <p class="lead"><?php print $site_slogan; ?></p>
       <?php endif; ?>
+      <?php print render($page['banner']); ?>
 
       <?php print render($page['header']); ?>
     </header> <!-- /#page-header -->
